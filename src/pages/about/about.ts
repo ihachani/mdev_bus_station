855c2aec51d39df4d835da69853ab7976277c4e1
ihachani <ihachaniuploads@gmail.com>
2016-11-30 02:02:03 +0100

    Added SearchBar to filter the results

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -15,6 +15,7 @@ export class AboutPage {
   @ViewChild('map') mapElement: ElementRef;
   map: any;
   private stationsInfo: MapInfo[];
+  private fullStationsList: MapInfo[];
   private rows;
 
   constructor(public navCtrl: NavController,
@@ -29,9 +30,30 @@ export class AboutPage {
   ionViewDidLoad() {
     this.stationsService.getStations().then(stations => {
       this.stationsInfo = stations;
-      this.rows = Array.from(Array(Math.ceil(this.stationsInfo.length / 2)).keys())
+      this.fullStationsList = stations;
+      this.createViewRows();
     });
+
   }
 
+  private createViewRows() {
+    this.rows = Array.from(Array(Math.ceil(this.stationsInfo.length / 2)).keys())
+  }
 
+  filterItems(ev: any) {
+    // set val to the value of the searchbar
+    let val = ev.target.value.trim();
+
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      this.stationsInfo = this.fullStationsList.filter((item) => {
+        return (item.name.toUpperCase().indexOf(val.toUpperCase()) >= 0) ||
+          (item.address.toUpperCase().indexOf(val.toUpperCase()) >= 0);
+      });
+      this.createViewRows();
+    } else {
+      this.stationsInfo = this.fullStationsList;
+      this.createViewRows();
+    }
+  }
 }
